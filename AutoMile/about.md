8be06ee41509fc90cb180914b9be0873ee0b4e16
Venu Pisharody <vrpisharody@users.noreply.github.com>
2014-11-22 16:08:05 +0530

    Added an about tab to the application

---
+++ AutoMile/about.md
@@ -0,0 +1,23 @@
+Know Your Car's Fuel Efficiency:
+=================
+
+This application predicts a car's fuel efficiency based on the inputs provided by the user. It uses a truncated version of the **mtcars** dataset to run a regression analysis to predict an automobiles fuel efficiency. 
+
+User interface is divided into three panes: 
+
+1. Left of the screen houses the input pane for the user to provide inputs related to the car.
+2. Middle or center of the screen houses the prediction pane which calculates and shows predictions.
+3. Right of the screen houses the information pane, which provides a brief description of the application and method to use it.
+
+All or any of the following inputs can be provided by the user to get a prediction of the automobile's fuel efficiency :
+
+1. Number of Cylinders in car. This is a drop down list box.
+2. Cylinder displacement of car. This is a slider input.
+3. Transmission, Auto or Manual. This is a drop down list box.
+4. Gross horse power. This is a slider input.
+5. Number of gears. This is a drop down list box.
+6. Weight of car in 1000 of lbs. This is a slider input.
+7. Number of carburettors. This ia a drop down list box.
+
+Once the user is sure of the inputs, user clicks the **Predict** button at the bottom of the input pane. Based on the inputs, the application calculates the car's fuel efficiency and shows **mpg (miles per gallon)** and **kmpl (kilometers per litre)**.
+
--- AutoMile/ui.R
+++ AutoMile/ui.R
@@ -3,102 +3,111 @@
 ### to predict an automobile's efficiency.
 
 library(shiny)
+library(markdown)
 
 #Load and subset required mtcars dataset
 data(mtcars)
 mcars=mtcars[,-c(5,7,8)]
 
-shinyUI(fluidPage(
-  
-  # Configure the title or header panel
-  wellPanel(h1("Know Your Car\'s Fuel Efficiency",align='center')),
-  
-  # Configure the input panel on the left side.
-  sidebarLayout(
-      
-      # Configure the header in the input panel.
-      sidebarPanel(
-               helpText(h4("Provide Your Car Information"),
-               strong("And"),
-               br(),
-               span(strong("We Will Give You Your Car\'s Efficiency"),style='color:blue'),
-               align='center'
+shinyUI(navbarPage("",
+                   
+  tabPanel("Know Your Car",
+       fluidPage(
+           # Configure the title or header panel
+           wellPanel(h1("Know Your Car\'s Fuel Efficiency",align='center')),
+           
+           # Configure the input panel on the left side.
+           sidebarLayout(
                
-        ),
-    
-      # Insert the input boxes.
-      selectInput("cyl", strong("Number Of Cylinders"), 
-                  choices=c(sort(unique(mcars$cyl))),selected=1),
-      sliderInput("disp", strong("Displacement - cu.in."), 
-                  min=min(mcars$disp),max=max(mcars$disp),value=mean(mcars$disp)),
-      selectInput("am", strong("Transmission"), 
-                  choices=list('Manual'=1,'Auto'=0),selected=0),
-      sliderInput("hp", strong("Horsepower"), 
-                  min=min(mcars$hp),max=max(mcars$hp),value=mean(mcars$hp)),
-      selectInput("gear", strong("Number Of Gears"), 
-                  choices=c(sort(unique(mcars$gear))),selected=4),
-      sliderInput("wt", strong("Weight Of The Car-1000 lbs"), 
-                  min=min(mcars$wt),max=max(mcars$wt),value=mean(mcars$wt)),
-      selectInput("carb", strong("Number Of Carburettors"), 
-                  choices=c(sort(unique(mcars$carb))),selected=1),
-      br(),
-      #Add submit button with proper spacing before and after it.
-      submitButton('Predict'),
-      br(),
-      br(),
-      br(),
-      br(),
-      offset=0
-      ),
-      
-    mainPanel(
-        # Split Main panel in to separate columns - for prediction and for information.
-        column(6,
-               p(em(strong('Read information in right side panel to run application')),style='color:red'),
-               br(),
-               br(),
-               br(),
-               # Add car image to have an attractive page.
-               img(src='Maserati.jpg'),
-               br(),
-               br(),
-               h3("Your Car\'s Predicted Efficiency Is",align='center'),
-               br(),
-               # Add output text for server
-               h4(textOutput('mileagempg'),style='color:red',align='center'),
-               h4("OR",align='center'),
-               h4(textOutput('mileagekmpl'),style='color:green',align='center'),
-               br(),
-               br(),
-               # Add horizontal rule line and help ful notes for the screen.
-               HTML("<hr style=\"border-color:#000000\" />"),
-               p(strong("Best Viewed in", span('Google Chrome',style='color:blue'),
-                 'with maximized Screen.', span('IE',style='color:blue'),
-                  "sometimes doesnot run scripts required to calculate")),
-               p("Set your display as required, if page doesnot show properly"),
-               offset=1
-        ),
-  
-        # The information column
-        column(4,
-               h3("Efficiency Predictor",style='color:blue'),
-                              
-               p('This application uses the',span(strong("mtcars"),style='color:red'),
-                 'dataset to predict an automobile\'s efficiency.',style='text-align:justify'),
-               p('The panel on the left accepts inputs from the user and uses a 
+               # Configure the header in the input panel.
+               sidebarPanel(
+                   helpText(h4("Provide Your Car Information"),
+                            strong("And"),
+                            br(),
+                            span(strong("We Will Give You Your Car\'s Efficiency"),style='color:blue'),
+                            align='center'
+                            
+                   ),
+                   
+                   # Insert the input boxes.
+                   selectInput("cyl", strong("Number Of Cylinders"), 
+                               choices=c(sort(unique(mcars$cyl))),selected=1),
+                   sliderInput("disp", strong("Displacement - cu.in."), 
+                               min=min(mcars$disp),max=max(mcars$disp),value=mean(mcars$disp)),
+                   selectInput("am", strong("Transmission"), 
+                               choices=list('Manual'=1,'Auto'=0),selected=0),
+                   sliderInput("hp", strong("Horsepower"), 
+                               min=min(mcars$hp),max=max(mcars$hp),value=mean(mcars$hp)),
+                   selectInput("gear", strong("Number Of Gears"), 
+                               choices=c(sort(unique(mcars$gear))),selected=4),
+                   sliderInput("wt", strong("Weight Of The Car-1000 lbs"), 
+                               min=min(mcars$wt),max=max(mcars$wt),value=mean(mcars$wt)),
+                   selectInput("carb", strong("Number Of Carburettors"), 
+                               choices=c(sort(unique(mcars$carb))),selected=1),
+                   br(),
+                   #Add submit button with proper spacing before and after it.
+                   submitButton('Predict'),
+                   br(),
+                   br(),
+                   br(),
+                   br(),
+                   offset=0
+               ),
+               
+               mainPanel(
+                   # Split Main panel in to separate columns - for prediction and for information.
+                   column(6,
+                          p(em(strong('Read information in right side panel to run application')),style='color:red'),
+                          br(),
+                          br(),
+                          br(),
+                          # Add car image to have an attractive page.
+                          img(src='Maserati.jpg'),
+                          br(),
+                          br(),
+                          h3("Your Car\'s Predicted Efficiency Is",align='center'),
+                          br(),
+                          # Add output text for server
+                          h4(textOutput('mileagempg'),style='color:red',align='center'),
+                          h4("OR",align='center'),
+                          h4(textOutput('mileagekmpl'),style='color:green',align='center'),
+                          br(),
+                          br(),
+                          # Add horizontal rule line and help ful notes for the screen.
+                          HTML("<hr style=\"border-color:#000000\" />"),
+                          p(strong("Best Viewed in", span('Google Chrome',style='color:blue'),
+                                   'with maximized Screen.', span('IE',style='color:blue'),
+                                   "sometimes doesnot run scripts required to calculate")),
+                          p("Set your display as required, if page doesnot show properly"),
+                          offset=1
+                   ),
+                   
+                   # The information column
+                   column(4,
+                          h3("Efficiency Predictor",style='color:blue'),
+                          
+                          p('This application uses the',span(strong("mtcars"),style='color:red'),
+                            'dataset to predict an automobile\'s efficiency.',style='text-align:justify'),
+                          p('The panel on the left accepts inputs from the user and uses a 
             regression model to predict an automobile\'s fuel efficiency in
             miles per gallon (mpg) and kilometers per litre (kmpl).',style='text-align:justify'),
-               p('Specification on',span(strong('number of cylinders, displacement, horsepower'),
-                                         style='color:red'),"etc are to be entered by the user in the left 
+                          p('Specification on',span(strong('number of cylinders, displacement, horsepower'),
+                                                    style='color:red'),"etc are to be entered by the user in the left 
             input panel.",code("sliderInput"),"and",code("selectInput"),"widgets 
             are used. Once the user has entered all required information, the user
             presses the", span(strong("\"Predict\""),style='color:blue'),'button at the
             bottom of the panel. The application then calculates and predicts 
             the automobile\'s fuel efficiency in the center panel.',style='text-align:justify'),
-            br(),
-            img(src='carcollage.jpg'),
-            offset=1   
-       )
-    )
-  )
+                          p(strong(em('Use the')),span(strong(em("About")),style='color:blue'),strong(em("tab to know
+            more on the application"))),
+                          br(),
+                          img(src='carcollage.jpg'),
+                          offset=1   
+                   )
+               )
+           )
+        )
+    ),
+    tabPanel("About",
+             includeMarkdown("about.md"))
 ))
